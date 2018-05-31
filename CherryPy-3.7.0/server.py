044e71a86b58402e7a4590531db33e40678c46b3
gxu630 <cannonlake>
2018-06-01 11:27:34 +1200

    third

--- CherryPy-3.7.0/server.py
+++ CherryPy-3.7.0/server.py
@@ -11,7 +11,10 @@
 #            Python  (We use 2.7)
 
 # The address we listen for connections on
+
+#listen_ip = "172.23.50.154"
 listen_ip = "0.0.0.0"
+#listen_port = 10007
 listen_port = 1234
 
 import cherrypy
@@ -19,7 +22,9 @@ from urllib import urlopen
 import hashlib
 import sqlite3
 import json
-import ChatHandler
+import mimetypes
+import os
+from bottle import route, run, template
 
 
 
@@ -42,20 +47,10 @@ class MainApp(object):
     # PAGES (which return HTML that can be viewed in browser)
     @cherrypy.expose
     def index(self):
-        #Page = "Welcome! This is a test website for COMPSYS302!<br/>"
-        Page = open("startscreen.html", "r")
 
-        #try:
-            #Page += "Hello " + cherrypy.session['username'] + "!<br/>"
-            #Page += "Here is some bonus text because you've logged in!"
-        #except KeyError: #There is no username
-            
-            #Page += "Click here to <a href='login'>login</a>."
-        return Page
-        
-    @cherrypy.expose
-    def login(self):
-        Page = open("Login_v11/index.html", "r")
+        Page = open("LoginScreen/login.htm", "r")
+
+        #Page = open("Loginscreen.html", "r")
 
         return Page
 
@@ -65,66 +60,82 @@ class MainApp(object):
 
         myusername = cherrypy.session.get('username')
         EncryptedSaltedPassword = cherrypy.session.get('encryptedsaltedpassword')
-        Pageoriginal = urlopen("http://cs302.pythonanywhere.com/getList?username=" + myusername.lower() + "&password=" + EncryptedSaltedPassword + "&json=1").read()
+        Page = urlopen("http://cs302.pythonanywhere.com/getList?username=" + myusername.lower() + "&password=" + EncryptedSaltedPassword + "&json=1").read()
+        Allusers = urlopen("http://cs302.pythonanywhere.com/listUsers").read()
         #Page += open("chat.html", "r")
 
-        PageDecoded = Pageoriginal.decode('utf-8')
-        json2 = json.loads(PageDecoded)
+        jsonloaded = json.loads(Page)
+        Allusers = Allusers.split(',')
 
-
+        #os.remove("onlineusers.db")
         conn = sqlite3.connect('onlineusers.db')
         c = conn.cursor()
-        c.execute('CREATE TABLE IF NOT EXISTS stuffToPlot (username TEXT)')
+        c.execute('CREATE TABLE IF NOT EXISTS stuffToPlot (username TEXT, ip REAL, location REAL, lastLogin REAL, port REAL, publicKey REAL, onlineStatus REAL)')
+        c.execute('create unique index if not exists IndexUnique on stuffToPlot ( username )')
 
-        for username in json2:
-            print json2[username]
-            print json2[username]["username"]
+        for values in Allusers:
+            #updates database with all users
+            c.execute('''INSERT or ignore INTO stuffToPlot (username) VALUES (?)''', (values,))
 
-            c.execute("INSERT INTO stuffToPlot VALUES (?)", (json2[username]["username"],))
+        #sets online status to 0
+        c.executemany('''UPDATE stuffToPlot SET onlineStatus= ?''', '0')
+
+        for key,value in jsonloaded.items():
+            #sets online status to 0 as default
+            try: value['publicKey']
+            except KeyError:
+                value ['publicKey'] = 0
+
+            #updates information from currently online users and sets online = 1
+            c.execute('''UPDATE stuffToPlot SET ip = ?, location = ?, lastLogin = ?, port = ?, publicKey = ?, onlineStatus = ? where username = ?''', (value['ip'], value['location'], value['lastLogin'], value['port'], value['publicKey'], 1, value['username']))
             conn.commit()
 
-        return PageDecoded
+
+
+        data = c.fetchall()
+        response = template('onlineusers.db', rows=data)
+
+        data2 = c.fetchall()
+        response += template('onlineusers.db', rows=data2)
+
+        return response
 
 
 
+    @cherrypy.expose
+    def testprint(self):
+        print 'testprint'
+        Page = open("Loginscreen.html", "r")
+        return Page
 
-    @cherrypy.expose    
-    def sum(self, a=0, b=0): #All inputs are strings by default
-        output = int(a)+int(b)
-        return str(output)
-        
     # LOGGING IN AND OUT
     @cherrypy.expose
     def signin(self, username=None, password=None):
-        """Check their name and password and send them either to the main page, or back to the main login screen."""
-        #global user
-        #user = username
-        #global EncryptedSaltedPassword
+        #Takes username and password, encrypts it, checks it, if successful, redirects to chat page, if not refreshes current page
         EncryptedSaltedPassword = self.encrypt_string(password.lower()+username.lower())
         error = self.authoriseUserLogin(username,EncryptedSaltedPassword)
+
         if (error == 0):
             cherrypy.session['username'] = username;
             cherrypy.session['encryptedsaltedpassword'] = EncryptedSaltedPassword;
-            #cherrypy.session['username'] = username;
-            #raise cherrypy.HTTPRedirect('/chat')
             raise cherrypy.HTTPRedirect('/chat')
         else:
-            raise cherrypy.HTTPRedirect('/login')
+            raise cherrypy.HTTPRedirect('/index')
 
     @cherrypy.expose
     def signout(self):
         """Logs the current user out, expires their session"""
         username = cherrypy.session.get('username')
-        if (username == None):
-            pass
-        else:
-            cherrypy.lib.sessions.expire()
-        raise cherrypy.HTTPRedirect('/')
+        EncryptedSaltedPassword = cherrypy.session.get('encryptedsaltedpassword')
+        Page = urlopen("http://cs302.pythonanywhere.com/logoff?username=" + username.lower() + "&password=" + EncryptedSaltedPassword).read()
+        cherrypy.lib.sessions.expire()
+        raise cherrypy.HTTPRedirect('/index')
+
         
     def authoriseUserLogin(self, username, EncryptedSaltedPassword):
+        #This contacts the server and validates credentials, returns validation code
         print username
         print EncryptedSaltedPassword
-        #if (username.lower() == "gxu630") and (password.lower() == "mainrabbit"):
         Page = urlopen("http://cs302.pythonanywhere.com/report?username=" + username.lower() + "&password=" + EncryptedSaltedPassword + "&location=1&ip=202.36.244.6&port=1001").read()
         if Page == "0, User and IP logged":
             return 0
@@ -132,48 +143,17 @@ class MainApp(object):
             return 1
 
     def encrypt_string(self, hash_string):
+        #this encrypts a string
         sha_signature = hashlib.sha256(hash_string.encode()).hexdigest()
         return sha_signature
 
     @cherrypy.expose
-    def css(self, filename):
-        f = open("Login_v11/css/" + filename, "r")
-        data = f.read()
-        print data
-        f.close()
-        #return correct mimetype
-        return data
-
-    @cherrypy.expose
-    def vendor(self, dir1, dir2, dir3, filename):
-        f = open("Login_v11/vendor/" + dir1+"/"+dir2+"/"+dir3+"/"+filename, "r")
+    def Login_files(self, filename):
+        f = open("LoginScreen/Login_files/" + filename, "r")
         data = f.read()
         f.close()
         #return correct mimetype
-        return data
-
-    @cherrypy.expose
-    def fonts(self, filename):
-        f = open("Login_v11/fonts/" + filename, "r")
-        data = f.read()
-        f.close()
-        #return correct mimetype
-        return data
-
-    @cherrypy.expose
-    def images(self, filename):
-        f = open("Login_v11/images/" + filename, "r")
-        data = f.read()
-        f.close()
-        #return correct mimetype
-        return data
-
-    @cherrypy.expose
-    def js(self, filename):
-        f = open("Login_v11/js/" + filename, "r")
-        data = f.read()
-        f.close()
-        #return correct mimetype
+        cherrypy.response.headers['Content-Type'] = mimetypes.guess_type(filename)[0]
         return data
           
 def runMainApp():
